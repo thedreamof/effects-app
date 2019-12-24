65e4473e6ca9e1cccce4f10d22e1a346bb86c28d
thedreamof <thedreamofbeast@hotmail.com>
2019-12-24 14:03:04 -0500

    primer comit app sin effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
+
+// Modulos personalizados
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +16,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    UsuariosModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
